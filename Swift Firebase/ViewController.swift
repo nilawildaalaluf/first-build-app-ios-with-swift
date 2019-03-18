443c3eccedcd17d0aa443ed8290727b9cc7b0596
_nilawilda <nila@cocowork.co>
2019-03-18 14:46:57 +0700

    Initial Commit

---
+++ Swift Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift Firebase
+//
+//  Created by irobot on 18/03/19.
+//  Copyright © 2019 enstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
